9af4a1fc1a0ced3a672f1cb06883598cc4f3a769
Dany Tamara <dany.tamara93@gmail.com>
2022-08-18 23:27:50 -0500

    commit 2

--- src/main/java/com/idat/EFDanyTamaraServicioPizza/EfDanyTamaraServicioPizzaApplication.java
+++ src/main/java/com/idat/EFDanyTamaraServicioPizza/EfDanyTamaraServicioPizzaApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
+import com.idat.EFDanyTamaraServicioPizza.client.OpenFeignCliente;
+import com.idat.EFDanyTamaraServicioPizza.client.OpenFeignPizzeria;
+
+@EnableFeignClients(basePackageClasses = {OpenFeignCliente.class,OpenFeignPizzeria.class})
 @SpringBootApplication
 public class EfDanyTamaraServicioPizzaApplication {
 
--- src/main/java/com/idat/EFDanyTamaraServicioPizza/service/PizzeriaPizzaServiceImpl.java
+++ src/main/java/com/idat/EFDanyTamaraServicioPizza/service/PizzeriaPizzaServiceImpl.java
@@ -3,6 +3,7 @@ package com.idat.EFDanyTamaraServicioPizza.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.idat.EFDanyTamaraServicioPizza.client.OpenFeignPizzeria;
 import com.idat.EFDanyTamaraServicioPizza.dto.PizzeriaDTO;
@@ -10,6 +11,7 @@ import com.idat.EFDanyTamaraServicioPizza.entity.PizzeriaPizza;
 import com.idat.EFDanyTamaraServicioPizza.entity.PizzeriaPizzaFK;
 import com.idat.EFDanyTamaraServicioPizza.repository.PizzeriaPizzaRepository;
 
+@Service
 public class PizzeriaPizzaServiceImpl implements PizzeriaPizzaService {
 
 	@Autowired
